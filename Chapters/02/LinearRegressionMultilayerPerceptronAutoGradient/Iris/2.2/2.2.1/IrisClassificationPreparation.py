5818be1e7809d486f065d525bc3cd3d8abe84cce
LI, Hui <pub@halfroad.com>
2022-10-29 17:04:25 +0800

    Iris Classification Preparation

---
+++ Chapters/02/LinearRegressionMultilayerPerceptronAutoGradient/Iris/2.2/2.2.1/IrisClassificationPreparation.py
@@ -0,0 +1,26 @@
+import jax
+from sklearn import datasets
+
+
+def setup():
+
+    iris = datasets.load_iris()
+
+    data = jax.numpy.float32(iris.data)
+    targets = jax.numpy.float32(iris.target)
+
+    return data, targets
+
+def main():
+
+    data, targets = setup()
+
+    # print(f"Data: {data[: 15]}")
+    print("----------------------")
+    print(f"Target: {targets}")
+
+if __name__ == "__main__":
+
+    main()
+
+
