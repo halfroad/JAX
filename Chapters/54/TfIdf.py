d1c6c0012d4453bd7efcfe1739e9081d33c22b3a
李进辉 <jinhui@JinHui.halfroad.com>
2023-06-13 21:37:27 +0800

    Initial commit

--- Chapters/54/TfIdf.py
+++ Chapters/54/TfIdf.py
@@ -12,73 +12,7 @@ import gensim
 
 sys.path.append("../52/")
 
-class TfIdf:
-    
-    def __init__(self, corpus, model = None):
-        
-        self.corpus = corpus
-        self.model = model
-        self.idfs = self.__idf()
-        
-    def __idf(self):
-        
-        dfi = {}
-        N = 0.0
-        
-        # Number of occurences
-        for document in self.corpus:
-            
-            N += 1
-            
-            counted = []
-            
-            for word in document:
-                
-                if word not in counted:
-                    
-                    counted.append(word)
-                    
-                    if word in dfi:
-                        
-                        dfi[word] += 1
-                        
-                    else:
-                        
-                        dfi[word] = 1
-        
-        idfs = {}
-        
-        for word in dfi:
-            
-            idfs[word] = jax.numpy.log(N / float(dfi[word]))
-            
-        return idfs
-    
-    def tfIdf(self):
-        
-        tfs = {}
-        tfidf_strings = []
-        
-        for document in self.corpus:
-            
-            word_occurences_in_given_document = {}
-            
-            for word in document:
-                
-                if word in tfs:
-                    word_occurences_in_given_document[word] += 1
-                else:
-                    word_occurences_in_given_document[word] = 1
-            
-            for word in tfs:
-                
-                word_occurences_in_given_document[word] *= self.idfs[word]
-                sorted_values = sorted(word_occurences_in_given_document.items(), key = lambda  item: item[1], reverse = True)
-                sorted_values = [value[0] for value in sorted_values]
-                
-                tfidf_strings.append(sorted_values)
-                
-        return tfidf_strings
+import AgNewsCsvReader
 
 class LossLogger(gensim.models.callbacks.CallbackAny2Vec):
     
@@ -114,7 +48,69 @@ class LossLogger(gensim.models.callbacks.CallbackAny2Vec):
     def on_train_end(self, model):
         
         print("Train ended")
+
+def idf(corpus):
+    
+    dfi = {}
+    N = 0.0
+    
+    # Number of occurences
+    for document in corpus:
         
+        N += 1
+        
+        counted = []
+        
+        for word in document:
+            
+            if word not in counted:
+                
+                counted.append(word)
+                
+                if word in dfi:
+                    
+                    dfi[word] += 1
+                    
+                else:
+                    
+                    dfi[word] = 1
+    
+    idfs = {}
+    
+    for word in dfi:
+        
+        idfs[word] = jax.numpy.log(N / float(dfi[word]))
+        
+    return idfs
+
+def tfidf(corpus):
+    
+    tfs = {}
+    tfidf_strings = []
+    
+    for document in corpus:
+        
+        word_occurences_in_given_document = {}
+        
+        for word in document:
+            
+            if word in tfs:
+                word_occurences_in_given_document[word] += 1
+            else:
+                word_occurences_in_given_document[word] = 1
+        
+        idfs = idf(corpus)
+        
+        for word in tfs:
+            
+            word_occurences_in_given_document[word] *= idfs[word]
+            sorted_values = sorted(word_occurences_in_given_document.items(), key = lambda  item: item[1], reverse = True)
+            sorted_values = [value[0] for value in sorted_values]
+            
+            tfidf_strings.append(sorted_values)
+            
+    return tfidf_strings
+
 def train():
     
     labels, titles, descriptions = AgNewsCsvReader.setup()
@@ -123,9 +119,7 @@ def train():
     
     callback = LossLogger()
     
-    tfIdf = TfIdf(descriptions)
-    
-    descriptions = tfIdf.tfIdf()
+    descriptions = tfidf(descriptions)
     
     model = Word2VecGenSim.retrain(descriptions, name, callback)
    
